ea3cc4f5ed0ab1ba4931feae1283d9558f620ea9
Hugh-Nguyen1_iag <Hugh.Nguyen1@iag.com.au>
2025-03-09 09:48:01 +1100

    tf stuff

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,16 +1,18 @@
-
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "3.14.2"
+  version = "5.8.1" # Latest stable version
 
-  name           = "eks-vpc"
-  cidr           = "10.0.0.0/16"
-  azs            = ["ap-southeast-2a", "ap-southeast-2b"]
-  public_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  private_subnets= ["10.0.101.0/24", "10.0.102.0/24"]
+  name                 = "eks-vpc"
+  cidr                 = "10.0.0.0/16"
+  azs                  = ["ap-southeast-2a", "ap-southeast-2b"] # Adjust as needed
+  private_subnets = ["10.0.101.0/24", "10.0.102.0/24"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
 
   enable_nat_gateway   = true
   single_nat_gateway   = true
+
+  enable_dns_hostnames = true
+  enable_dns_hostnames = true
 }
 
 resource "aws_eks_cluster" "main" {
