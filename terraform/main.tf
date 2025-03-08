2358ea617ce926a484c6aed66d24e6b883551e5f
Hugh-Nguyen1_iag <Hugh.Nguyen1@iag.com.au>
2025-03-08 15:41:26 +1100

    tf update

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,8 +12,8 @@ module "vpc" {
   public_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets= ["10.0.101.0/24", "10.0.102.0/24"]
 
-  enable_nat_gateway = true
-  single_nat_gateway = true
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
 }
 
 module "eks" {
@@ -24,8 +24,6 @@ module "eks" {
   cluster_version = "1.28"
   vpc_id          = module.vpc.vpc_id
   subnet_ids      = module.vpc.private_subnets
-
-  create_node_role = true
 
   eks_managed_node_groups = {
     example = {
