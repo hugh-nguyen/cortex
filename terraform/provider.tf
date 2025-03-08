abf09fc201bc9adad841af722d4f4e07bba115f5
Hugh-Nguyen1_iag <Hugh.Nguyen1@iag.com.au>
2025-03-09 09:29:37 +1100

    tf stuff

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,6 +4,13 @@ terraform {
     kubernetes = { source = "hashicorp/kubernetes", version = "~> 2.0" }
     helm = { source = "hashicorp/helm", version = "~> 2.0" }
   }
+  backend "s3" {
+    bucket         = "mapleharbour-tf-state"
+    key            = "envoy-service-hub/terraform.tfstate"
+    region         = "ap-southeast-2"
+    encrypt        = true
+    dynamodb_table = "terraform-lock"
+  }
 }
 
 provider "aws" {
