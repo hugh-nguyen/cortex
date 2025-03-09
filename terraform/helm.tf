7d4a3cd7a7b2e4386bbc8572f9300c7c800ea313
Hugh-Nguyen1_iag <Hugh.Nguyen1@iag.com.au>
2025-03-09 16:12:28 +1100

    tf stuff

---
+++ terraform/helm.tf
@@ -0,0 +1,53 @@
+provider "helm" {
+  kubernetes {
+    host                   = aws_eks_cluster.main.endpoint
+    token                  = data.aws_eks_cluster_auth.main.token
+    cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
+  }
+}
+
+resource "aws_iam_role" "aws_lb_controller_role" {
+  name = "aws-load-balancer-controller-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "aws_lb_controller_policy" {
+  role       = aws_iam_role.aws_lb_controller_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSLoadBalancerControllerIAMPolicy"
+}
+
+resource "kubernetes_service_account" "aws_lb_controller" {
+  metadata {
+    name      = "aws-load-balancer-controller"
+    namespace = "kube-system"
+  }
+}
+
+resource "kubernetes_role_binding" "aws_lb_controller_binding" {
+  metadata {
+    name      = "aws-load-balancer-controller"
+    namespace = "kube-system"
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "Role"
+    name      = "aws-load-balancer-controller"
+  }
+
+  subjects {
+    kind      = "ServiceAccount"
+    name      = "aws-load-balancer-controller"
+    namespace = "kube-system"
+  }
+}
