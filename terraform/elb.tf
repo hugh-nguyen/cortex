7faf3e1af8c7d32c026bea3128864185cc44eca9
Hugh-Nguyen1_iag <Hugh.Nguyen1@iag.com.au>
2025-03-09 12:56:13 +1100

    tf stuff

--- terraform/elb.tf
+++ terraform/elb.tf
@@ -1,32 +1,32 @@
 resource "aws_lb" "eks_lb" {
   name               = "eks-lb"
   internal           = false
-  load_balancer_type = "network"
+  load_balancer_type = "application"
   security_groups    = [aws_security_group.lb_sg.id]
   subnets            = module.vpc.public_subnets
-
   enable_deletion_protection = false
 }
 
 resource "aws_lb_target_group" "eks_tg" {
   name     = "eks-tg"
   port     = 80
-  protocol = "TCP"
+  protocol = "HTTP"  # Changing to HTTP since ALB supports it
   vpc_id   = module.vpc.vpc_id
 
   health_check {
-    protocol            = "TCP"
+    path                = "/health"
     interval            = 30
     timeout             = 5
     healthy_threshold   = 2
     unhealthy_threshold = 2
+    matcher             = "200"
   }
 }
 
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.eks_lb.arn
   port              = 80
-  protocol          = "TCP"
+  protocol          = "HTTP"
 
   default_action {
     type             = "forward"
