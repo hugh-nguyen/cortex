c9475b70357251b16cfdb9c0aed139855ccc2630
Hugh-Nguyen1_iag <Hugh.Nguyen1@iag.com.au>
2025-03-09 19:39:58 +1100

    tf stuff

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -64,12 +64,6 @@ resource "aws_eks_node_group" "main" {
   }
 
   instance_types = ["t3.micro"]
-
-  launch_template {
-    id      = aws_launch_template.eks_nodes.id
-    version = "$Latest"
-  }
-
   depends_on = [aws_eks_cluster.main]
 }
 
