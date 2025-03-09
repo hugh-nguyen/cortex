6d557f516422ac055770c9b39a1a802dfa41994a
Hugh-Nguyen1_iag <Hugh.Nguyen1@iag.com.au>
2025-03-09 17:42:28 +1100

    tf stuff

--- terraform/helm.tf
+++ terraform/helm.tf
@@ -3,13 +3,67 @@ data "aws_eks_cluster" "main" {
 }
 
 data "tls_certificate" "eks_oidc" {
-  url = data.aws_eks_cluster.main.identity[0].oidc[0].issuer
+  url = trimsuffix(data.aws_eks_cluster.main.identity[0].oidc[0].issuer, "/")
+}
+
+data "aws_vpc" "eks_vpc" {
+  id = data.aws_eks_cluster.main.vpc_config[0].vpc_id
 }
 
 resource "aws_iam_openid_connect_provider" "eks_oidc_provider" {
   url             = data.tls_certificate.eks_oidc.url
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = [data.tls_certificate.eks_oidc.certificates[0].sha1_fingerprint]
+}
+
+resource "aws_iam_policy" "aws_lb_controller_policy" {
+  name        = "AWSLoadBalancerControllerIAMPolicy"
+  description = "IAM policy for AWS Load Balancer Controller"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = [
+          "ec2:DescribeInstances",
+          "ec2:DescribeTags",
+          "ec2:DescribeVpcs",
+          "ec2:DescribeSubnets",
+          "ec2:DescribeSecurityGroups",
+          "ec2:AuthorizeSecurityGroupIngress",
+          "ec2:RevokeSecurityGroupIngress",
+          "ec2:CreateSecurityGroup",
+          "ec2:DeleteSecurityGroup",
+          "ec2:CreateTags",
+          "ec2:DeleteTags",
+          "elasticloadbalancing:DescribeLoadBalancers",
+          "elasticloadbalancing:DescribeListeners",
+          "elasticloadbalancing:DescribeTargetGroups",
+          "elasticloadbalancing:DescribeTargetHealth",
+          "elasticloadbalancing:DescribeTags",
+          "elasticloadbalancing:RegisterTargets",
+          "elasticloadbalancing:DeregisterTargets"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "elasticloadbalancing:CreateListener",
+          "elasticloadbalancing:DeleteListener",
+          "elasticloadbalancing:CreateLoadBalancer",
+          "elasticloadbalancing:DeleteLoadBalancer",
+          "elasticloadbalancing:CreateTargetGroup",
+          "elasticloadbalancing:DeleteTargetGroup",
+          "elasticloadbalancing:ModifyLoadBalancerAttributes",
+          "elasticloadbalancing:ModifyTargetGroup",
+          "elasticloadbalancing:ModifyTargetGroupAttributes"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
 }
 
 resource "aws_iam_role" "aws_lb_controller_role" {
@@ -25,16 +79,16 @@ resource "aws_iam_role" "aws_lb_controller_role" {
       Action = "sts:AssumeRoleWithWebIdentity"
       Condition = {
         StringEquals = {
-          "${data.aws_eks_cluster.main.identity[0].oidc[0].issuer}:sub" = "system:serviceaccount:kube-system:aws-load-balancer-controller"
+          "${trimsuffix(data.aws_eks_cluster.main.identity[0].oidc[0].issuer, "/")}:sub" = "system:serviceaccount:kube-system:aws-load-balancer-controller"
         }
       }
     }]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "aws_lb_controller_policy" {
+resource "aws_iam_role_policy_attachment" "aws_lb_controller_policy_attachment" {
   role       = aws_iam_role.aws_lb_controller_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+  policy_arn = aws_iam_policy.aws_lb_controller_policy.arn
 }
 
 resource "kubernetes_service_account" "aws_lb_controller" {
@@ -45,6 +99,8 @@ resource "kubernetes_service_account" "aws_lb_controller" {
       "eks.amazonaws.com/role-arn" = aws_iam_role.aws_lb_controller_role.arn
     }
   }
+
+  depends_on = [aws_iam_role_policy_attachment.aws_lb_controller_policy_attachment]
 }
 
 resource "kubernetes_cluster_role_binding" "aws_lb_controller_binding" {
@@ -91,4 +147,16 @@ resource "helm_release" "aws_lb_controller" {
     name  = "region"
     value = "ap-southeast-2"
   }
+
+  set {
+    name  = "vpcId"
+    value = data.aws_vpc.eks_vpc.id
+  }
+
+  set {
+    name  = "debug"
+    value = "true"
+  }
+
+  depends_on = [kubernetes_service_account.aws_lb_controller]
 }
