c9ad3e9aa9ee0e3dc1b188ff288670af2565cfda
Hugh-Nguyen1_iag <Hugh.Nguyen1@iag.com.au>
2025-03-09 21:50:25 +1100

    tf stuff

--- terraform/elb.tf
+++ terraform/elb.tf
@@ -6,3 +6,22 @@ resource "aws_lb" "eks_lb" {
   subnets            = module.vpc.public_subnets
   enable_deletion_protection = false
 }
+
+resource "aws_lb_target_group" "eks_tg" {
+  name     = "eks-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = module.vpc.vpc_id
+
+  target_type = "ip"
+
+  health_check {
+    path                = "/health"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200"
+  }
+}
+
