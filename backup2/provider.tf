ec1ad73b0974a4bdbe1d4f63926cdd2f798eb019
Hugh-Nguyen1_iag <Hugh.Nguyen1@iag.com.au>
2025-03-09 21:18:05 +1100

    tf stuff

--- backup2/provider.tf
+++ backup2/provider.tf
@@ -18,7 +18,8 @@ provider "aws" {
 }
 
 data "aws_eks_cluster_auth" "main" {
-  name = "cluster"
+  name = aws_eks_cluster.main.name
+  depends_on = [aws_eks_cluster.main] 
 }
 
 provider "kubernetes" {
