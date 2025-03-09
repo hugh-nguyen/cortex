c611d264d655474f55df1aba5e12e960ce5d6c4f
Hugh-Nguyen1_iag <Hugh.Nguyen1@iag.com.au>
2025-03-09 21:51:14 +1100

    tf stuff

--- terraform/elb.tf
+++ terraform/elb.tf
@@ -25,3 +25,14 @@ resource "aws_lb_target_group" "eks_tg" {
   }
 }
 
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.eks_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.eks_tg.arn
+  }
+}
