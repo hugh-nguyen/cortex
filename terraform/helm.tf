905f460463f2e21513f52ba5c80f804d9c592abf
Hugh-Nguyen1_iag <Hugh.Nguyen1@iag.com.au>
2025-03-09 16:13:58 +1100

    tf stuff

--- terraform/helm.tf
+++ terraform/helm.tf
@@ -1,10 +1,4 @@
-provider "helm" {
-  kubernetes {
-    host                   = aws_eks_cluster.main.endpoint
-    token                  = data.aws_eks_cluster_auth.main.token
-    cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
-  }
-}
+
 
 resource "aws_iam_role" "aws_lb_controller_role" {
   name = "aws-load-balancer-controller-role"
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -30,6 +30,7 @@ provider "kubernetes" {
 provider "helm" {
   kubernetes {
     host                   = aws_eks_cluster.main.endpoint
+    token                  = data.aws_eks_cluster_auth.main.token
     cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
