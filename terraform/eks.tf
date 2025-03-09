bdc612167a9149eaa6e03c45ea31ec9a58db7a38
Hugh-Nguyen1_iag <Hugh.Nguyen1@iag.com.au>
2025-03-09 19:37:08 +1100

    tf stuff

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -22,32 +22,33 @@ resource "aws_eks_cluster" "main" {
   }
 }
 
-resource "aws_launch_template" "eks_nodes" {
-  name_prefix   = "eks-nodes"
-  image_id      = data.aws_ami.eks_ami.id
-  instance_type = "t3.micro"
-
-  metadata_options {
-    http_tokens   = "optional"
-    http_endpoint = "enabled"
-  }
-
-  tag_specifications {
-    resource_type = "instance"
-    tags = {
-      Name = "eks-node"
-    }
-  }
+resource "aws_iam_openid_connect_provider" "eks_oidc_provider" {
+  url             = aws_eks_cluster.main.identity[0].oidc[0].issuer
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["<thumbprint>"]
 }
 
-data "aws_ami" "eks_ami" {
-  most_recent = true
-  owners      = ["602401143452"]  # AWS EKS AMI owner ID
+resource "aws_iam_role" "cluster_role" {
+  name = "eks-cluster-role"
 
-  filter {
-    name   = "name"
-    values = ["amazon-eks-node-*"]
-  }
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = { Service = "eks.amazonaws.com" }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  role       = aws_iam_role.cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_service_policy" {
+  role       = aws_iam_role.cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
 }
 
 resource "aws_eks_node_group" "main" {
@@ -69,7 +70,7 @@ resource "aws_eks_node_group" "main" {
     version = "$Latest"
   }
 
-  depends_on = [aws_iam_role_policy_attachment.node_policy]
+  depends_on = [aws_eks_cluster.main]
 }
 
 resource "aws_iam_role" "node" {
@@ -87,12 +88,7 @@ resource "aws_iam_role" "node" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "nodes_ecr" {
-  role       = aws_iam_role.node.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-}
-
-resource "aws_iam_role_policy_attachment" "node_worker_policy" {
+resource "aws_iam_role_policy_attachment" "node_policy" {
   role       = aws_iam_role.node.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
@@ -107,37 +103,30 @@ resource "aws_iam_role_policy_attachment" "node_ecr_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
-resource "aws_iam_role_policy_attachment" "node_policy" {
-  role       = aws_iam_role.node.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+provider "aws" {
+  region = "ap-southeast-2"
 }
 
-resource "aws_iam_role" "cluster_role" {
-  name = "eks-cluster-role"
+provider "kubernetes" {
+  host                   = aws_eks_cluster.main.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Service = "eks.amazonaws.com"
-      }
-      Action = "sts:AssumeRole"
-    }]
-  })
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.main.name]
+  }
 }
 
-resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
-  role       = aws_iam_role.cluster_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-}
-
-resource "aws_iam_role_policy_attachment" "eks_service_policy" {
-  role       = aws_iam_role.cluster_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
-}
-
-resource "aws_iam_role_policy_attachment" "admin_policy" {
-  role       = aws_iam_role.cluster_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+provider "helm" {
+  kubernetes {
+    host                   = aws_eks_cluster.main.endpoint
+    token                  = data.aws_eks_cluster_auth.main.token
+    cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.main.name]
+    }
+  }
 }
