333ebe6367d1797e8f39e8f08c596fa6b54b41fa
Hugh-Nguyen1_iag <Hugh.Nguyen1@iag.com.au>
2025-03-10 13:17:56 +1100

    tf stuff

--- backup3/elb.tf
+++ backup3/elb.tf
@@ -44,3 +44,26 @@ resource "aws_lb_listener" "http" {
     target_group_arn = aws_lb_target_group.eks_tg.arn
   }
 }
+
+# data "aws_lb" "eks_lb" {
+#   name = "eks-lb"
+# }
+
+# data "aws_lb_target_group" "eks_tg" {
+#   name = "eks-tg"  # Make sure this matches your existing target group
+# }
+
+data "aws_instances" "eks_nodes" {
+  filter {
+    name   = "tag:eks:nodegroup-name"
+    values = [aws_eks_node_group.main.node_group_name]
+  }
+}
+
+resource "aws_lb_target_group_attachment" "eks_nodes" {
+  count = length(data.aws_instances.eks_nodes.private_ips)
+
+  target_group_arn = aws_lb_target_group.eks_tg.arn
+  target_id        = data.aws_instances.eks_nodes.private_ips[count.index]
+  port             = 80
+}
