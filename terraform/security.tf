27ac34f85a03975afb054a7f8d91bbf2fc66a120
Hugh-Nguyen1_iag <Hugh.Nguyen1@iag.com.au>
2025-03-10 15:00:39 +1100

    tf stuff

--- terraform/security.tf
+++ terraform/security.tf
@@ -23,3 +23,26 @@ resource "aws_security_group" "lb_sg" {
     Name = "eks-lb-sg"
   }
 }
+
+data "aws_security_group" "eks_cluster_sg" {
+  filter {
+    name   = "vpc-id"
+    values = [module.vpc.vpc_id]
+  }
+
+  filter {
+    name   = "group-name"
+    values = ["eks-cluster-sg-cluster-*"]
+  }
+}
+
+resource "aws_security_group_rule" "allow_alb_to_eks_nodes" {
+  type                     = "ingress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  security_group_id        = data.aws_security_group.eks_cluster_sg.id
+  source_security_group_id = aws_security_group.lb_sg.id
+  description              = "Allow ALB to communicate with EKS worker nodes"
+}
+
