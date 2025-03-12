7fc6d5797c10b36c4dfc39c532ca94676f129333
Hugh-Nguyen1_iag <Hugh.Nguyen1@iag.com.au>
2025-03-12 11:43:48 +1100

    tf stuff

--- terraform/main/eks.tf
+++ terraform/main/eks.tf
@@ -39,8 +39,8 @@ resource "aws_eks_node_group" "main" {
   subnet_ids      = module.vpc.private_subnets
 
   scaling_config {
-    desired_size = 5
-    max_size     = 6
+    desired_size = 3
+    max_size     = 4
     min_size     = 1
   }
 
