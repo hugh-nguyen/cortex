561165f03842446d553783026b51a1174eb0445f
Hugh-Nguyen1_iag <Hugh.Nguyen1@iag.com.au>
2025-03-14 09:46:06 +1100

    deploying nexus

--- src/deploy-nexus.py
+++ src/deploy-nexus.py
@@ -1,10 +1,14 @@
-import yaml, os, subprocess
+import yaml, os, subprocess, argparse
 
 from util import *
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--clone', action='store_true')
 args = parser.parse_args()
+
+print("===========Reading Neuroverse===========")
+nv = yaml.safe_load(open("neuroverse.yaml", "r").read())
+services = nv["services"]
 
 if args.clone:
     print("===========Clone Repositories===========")
@@ -16,14 +20,9 @@ if args.clone:
         repo = f"{app}-iac"
         subprocess.run(["git", "clone", f"https://github.com/hugh-nguyen/{repo}.git", f"temp/{repo}"], check=True)
 
-print("===========Reading Neuroverse===========")
-nv = yaml.safe_load(open("neuroverse.yaml", "r").read())
-
-
 print("===========Connecting to EKS Cluster===========")
 subprocess.run(["aws", "eks", "update-kubeconfig", "--region", "ap-southeast-2", "--name", "cluster"], check=True)
 
-services = nv["services"]
 for s in services:
     app, svc, ver = s["app"], s["svc"], s["ver"]
     print("\n====Deploying", f"{app}-{svc}-{ver.replace('.', '-')}====")
