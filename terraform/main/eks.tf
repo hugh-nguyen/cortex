4a59d7dd15fa866d34322b3c50d60b7968654140
Hugh-Nguyen1_iag <Hugh.Nguyen1@iag.com.au>
2025-03-14 16:37:35 +1100

    deploy nexus

--- terraform/main/eks.tf
+++ terraform/main/eks.tf
@@ -4,9 +4,9 @@ module "vpc" {
 
   name                 = "eks-vpc"
   cidr                 = "10.0.0.0/16"
-  azs                  = ["ap-southeast-2b"]
-  private_subnets      = ["10.0.101.0/24"]
-  public_subnets       = ["10.0.1.0/24"]
+  azs                  = ["ap-southeast-2a", "ap-southeast-2b"]
+  private_subnets      = ["10.0.101.0/24", "10.0.102.0/24"]
+  public_subnets       = ["10.0.1.0/24", "10.0.2.0/24"]
 
   enable_nat_gateway   = true
   single_nat_gateway   = true
