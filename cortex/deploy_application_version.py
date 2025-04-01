524fe6f09aa9e0fd2424f7e77ce3157ab1a7787a
Hugh-Nguyen1_iag <Hugh.Nguyen1@iag.com.au>
2025-04-01 22:53:34 +1100

    envoy control plane

--- cortex/deploy_application_version.py
+++ cortex/deploy_application_version.py
@@ -79,9 +79,8 @@ def deploy_routes(path_to_deploy_log):
     
     url = "http://hn-cortex.click/api/v1/routes"
     payload = {"routes": transform_routes(routes)}
-    print(json.dumps(payload))
-    # response = requests.post(url, json=payload)
-    # print("!!",response.text)
+    response = requests.post(url, json=payload)
+    print("!!",response.text)
 
 
 if __name__ == '__main__':
@@ -108,5 +107,5 @@ if __name__ == '__main__':
     
     manifest = yaml.safe_load(open(path, "r").read())
 
-    # deploy_services(manifest["services"])
+    deploy_services(manifest["services"])
     deploy_routes(DEPLOY_LOG_PATH)
