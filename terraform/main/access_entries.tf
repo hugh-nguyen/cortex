6189ebaaad9b1aeb380729378deb701877674c47
Hugh-Nguyen1_iag <Hugh.Nguyen1@iag.com.au>
2025-04-17 12:09:26 +1000

    new appdashboard

--- terraform/main/access_entries.tf
+++ terraform/main/access_entries.tf
@@ -1,19 +1,30 @@
-data "aws_caller_identity" "current" {}
-
 data "aws_partition" "current" {}
+
+data "aws_caller_identity" "current" {}
 
 resource "aws_eks_access_entry" "admin_user" {
   cluster_name      = aws_eks_cluster.main.name
   principal_arn     = "arn:${data.aws_partition.current.partition}:iam::${data.aws_caller_identity.current.account_id}:user/admin"
-  type              = "STANDARD"       # other option is "SYSTEM" for bootstrap entries
+  type              = "STANDARD"
   user_name         = "admin"
   kubernetes_groups = ["system:masters"]
-  depends_on        = [ aws_eks_cluster.main ]
+
+  depends_on = [
+    aws_eks_cluster.main
+  ]
 }
 
 resource "aws_eks_access_policy_association" "admin_user_policy" {
   cluster_name   = aws_eks_cluster.main.name
   principal_arn  = aws_eks_access_entry.admin_user.principal_arn
   policy_arn     = "arn:${data.aws_partition.current.partition}:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-  depends_on     = [ aws_eks_access_entry.admin_user ]
+
+  # **NEW**: you must have at least one access_scope block
+  access_scope {
+    type = "cluster"
+  }
+
+  depends_on = [
+    aws_eks_access_entry.admin_user
+  ]
 }
