499373491a8128b8e47670766224d2db6d4b9a73
Hugh-Nguyen1_iag <Hugh.Nguyen1@iag.com.au>
2025-04-17 11:33:32 +1000

    new appdashboard

--- terraform/main/k8s.tf
+++ terraform/main/k8s.tf
@@ -12,34 +12,35 @@ resource "kubernetes_service_account" "ecr_access_sa" {
 # get your account ID for the ARN
 data "aws_caller_identity" "current" {}
 
-# patch the aws-auth ConfigMap so your IAM user is a Kubernetes admin
-resource "kubernetes_config_map" "aws_auth" {
-  metadata {
-    name      = "aws-auth"
-    namespace = "kube-system"
-  }
-
-  data = {
-    # this must include your node‑group role so you don't lose node access.
-    # aws_iam_role.node is the IAM role you attached to your aws_eks_node_group.
-    mapRoles = <<YAML
+resource "kubectl_manifest" "aws_auth_patch" {
+  manifest = {
+    apiVersion = "v1"
+    kind       = "ConfigMap"
+    metadata = {
+      name      = "aws-auth"
+      namespace = "kube-system"
+    }
+    data = {
+      # merge in all of your existing entries plus your user
+      mapRoles = <<YAML
 - rolearn: ${aws_iam_role.node.arn}
   username: system:node:{{EC2PrivateDNSName}}
   groups:
     - system:bootstrappers
     - system:nodes
+# add any other mapped roles here…
 YAML
 
-    # add your IAM user here so you become a `system:masters` in the cluster
-    mapUsers = <<YAML
-- userarn: arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/admin
-  username:   admin
+      mapUsers = <<YAML
+- userarn: arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/hughmapleharbour
+  username: hughmapleharbour
   groups:
     - system:masters
+# add any additional users here…
 YAML
+    }
   }
 
-  # make sure the cluster exists before we apply this
   depends_on = [
     aws_eks_cluster.main
   ]
