e7e144b2082b6a599fee5aa9408038dca7a574f5
Hugh-Nguyen1_iag <Hugh.Nguyen1@iag.com.au>
2025-04-17 12:30:48 +1000

    new appdashboard

--- terraform/main/k8s.tf
+++ terraform/main/k8s.tf
@@ -9,3 +9,34 @@ resource "kubernetes_service_account" "ecr_access_sa" {
   depends_on = [aws_eks_cluster.main]
 }
 
+resource "kubernetes_config_map" "aws_auth" {
+  metadata {
+    name      = "aws-auth"
+    namespace = "kube-system"
+  }
+
+  data = {
+    mapRoles = yamlencode([
+      {
+        rolearn  = aws_iam_role.node.arn
+        username = "system:node:{{EC2PrivateDNSName}}"
+        groups   = ["system:bootstrappers", "system:nodes"]
+      }
+    ])
+    
+    mapUsers = yamlencode([
+      {
+        userarn  = "arn:aws:iam::495599745704:user/admin"
+        username = "admin"
+        groups   = ["system:masters"]
+      },
+      {
+        userarn  = "arn:aws:iam::495599745704:root"
+        username = "admin"
+        groups   = ["system:masters"]
+      }
+    ])
+  }
+
+  depends_on = [aws_eks_cluster.main]
+}
