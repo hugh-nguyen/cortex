4741c76c1f5a9b969d418e3ce4313daf15121751
Hugh-Nguyen1_iag <Hugh.Nguyen1@iag.com.au>
2025-04-17 12:06:01 +1000

    new appdashboard

--- terraform/main/k8s.tf
+++ terraform/main/k8s.tf
@@ -9,32 +9,3 @@ resource "kubernetes_service_account" "ecr_access_sa" {
   depends_on = [aws_eks_cluster.main]
 }
 
-data "aws_caller_identity" "current" {}
-
-resource "kubectl_manifest" "aws_auth_patch" {
-  yaml_body = <<-YAML
-apiVersion: v1
-kind: ConfigMap
-metadata:
-  name: aws-auth
-  namespace: kube-system
-data:
-  mapRoles: |
-    - rolearn: ${aws_iam_role.node.arn}
-      username: system:node:{{EC2PrivateDNSName}}
-      groups:
-        - system:bootstrappers
-        - system:nodes
-    # …any other existing role mappings…
-  mapUsers: |
-    - userarn: arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/admin
-      username: admin
-      groups:
-        - system:masters
-    # …any other users you want to map…
-YAML
-
-  depends_on = [
-    aws_eks_cluster.main
-  ]
-}
